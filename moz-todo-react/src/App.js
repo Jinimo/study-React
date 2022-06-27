d9c3346aa0f104a74c9f8db3f55bcc17f720473d
Jinimo <sdc36625110@gmail.com>
2022-06-27 23:45:04 +0900

    index

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -2,24 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '강남 고기 맛집'; 
+  function 함수(){
+    return 100
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={`${process.env.PUBLIC_URL}/public_assets/reoicon.PNG`} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>    
+        <div style={ {color: 'green', fontSize: '30px'}}> 현진 개발 Blog </div> 
+      </div>
+      <img src = {logo}/>
+      <h3> { 함수() }  </h3>
+      <h4> { posts }  </h4>
+
     </div>
   );
 }
-
+ 
 export default App;
